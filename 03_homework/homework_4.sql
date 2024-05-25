83ef35cbe5fdc155c731ae9606dcb30eb8c872f1
SerhiyZv <skiev31@gmail.com>
2024-05-24 23:01:31 -0400

    Update for homework-4

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -58,3 +58,72 @@ SELECT *, Count() OVER win1 AS purchased_cnt
 FROM customer_purchases
 WINDOW win1 AS (PARTITION BY customer_id, product_id)
 ORDER BY customer_id, product_id
+
+-- String manipulations
+/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
+These are separated from the product name with a hyphen. 
+Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
+Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
+
+| product_name               | description |
+|----------------------------|-------------|
+| Habanero Peppers - Organic | Organic     |
+
+Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
+
+SELECT product_name,
+	CASE WHEN INSTR(product_name, '-') > 0 THEN Trim(SUBSTR(product_name, INSTR(product_name, '-') + 1)) END AS description
+FROM product
+
+
+/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
+
+
+SELECT product_size
+FROM product
+WHERE product_size REGEXP '\d+'
+
+-- UNION
+/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
+
+HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
+1) Create a CTE/Temp Table to find sales values grouped dates; 
+2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
+"best day" and "worst day"; 
+3) Query the second temp table twice, once for the best day, once for the worst day, 
+with a UNION binding them. */
+--1
+WITH T1 AS (
+	SELECT market_date, SUM(quantity * cost_to_customer_per_qty) AS total_sales
+	FROM customer_purchases
+	GROUP BY market_date
+), W AS (
+	SELECT *,  ROW_NUMBER() OVER win1 AS RowId
+	FROM T1
+	WINDOW win1 AS (ORDER BY total_sales)
+), B AS (
+	SELECT *,  ROW_NUMBER() over win1 as RowId
+	FROM T1
+	WINDOW win1 AS (ORDER BY total_sales Desc)
+)
+SELECT market_date, total_sales, 'Worst' Descr FROM W WHERE RowId = 1
+UNION 
+SELECT market_date, total_sales, 'Best' Descr FROM B WHERE RowId = 1
+
+--2
+WITH T1 AS (
+	SELECT market_date, SUM(quantity * cost_to_customer_per_qty) AS total_sales
+	FROM customer_purchases
+	GROUP BY market_date
+), W AS (
+	SELECT market_date, total_sales, 'Worst' Descr 
+	FROM T1 
+	ORDER BY total_sales Limit 1
+), B AS (
+	SELECT market_date, total_sales, 'Best' Descr 
+	FROM T1 
+	ORDER BY total_sales Desc Limit 1
+)
+SELECT * FROM W
+UNION 
+SELECT * FROM B
